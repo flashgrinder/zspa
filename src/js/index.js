f61448fbc6132629b75c67bfda6ce5f4184fc582
flashgrinder <flashgrinder92@gmail.com>
2024-06-24 13:13:20 +0400

    make page certifications.html

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,15 @@
 import mobileNav from './modules/mobile-nav.js';
 import articlesSlider from './modules/articles-slider';
 import gallerySlider from "./modules/gallery-slider";
+import zoomImg from "./modules/zoomImg";
+
 document.addEventListener('DOMContentLoaded', ()=> {
 
     mobileNav();
 
     gallerySlider();
     articlesSlider();
+    zoomImg();
 
 })
 
---
+++ src/js/modules/zoomImg.js
@@ -0,0 +1,8 @@
+import { Fancybox } from "@fancyapps/ui";
+import "@fancyapps/ui/dist/fancybox/fancybox.css";
+
+function zoomImg () {
+    Fancybox.bind('[data-fancybox]');
+}
+
+export default zoomImg;
